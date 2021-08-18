3768cef828d501138b035b13ffd7265e3782e665
user <goobncl@gmail.com>
2021-08-18 13:51:38 +0400

    first commit

---
+++ MultiThreadApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace MultiThreadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().DoTest();
+        }
+
+        void DoTest()
+        {
+            Thread t1 = new Thread(new ThreadStart(Run));
+            t1.Start();
+            Run();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Thread#{0}: Begin", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread#{0}: End", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
