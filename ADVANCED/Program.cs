b7bc7f8ed6665955b528c5457df577b600d68741
user <goobncl@gmail.com>
2021-08-18 16:53:57 +0400

    Reorganizing solution and projects.

---
+++ ADVANCED/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ADVANCED
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
